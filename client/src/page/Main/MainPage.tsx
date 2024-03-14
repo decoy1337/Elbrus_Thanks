79e938fcc48c49228d5c5e251fcab1e9b32dd27e
Vadim <bezvadimka@mail.ru>
2024-03-14 21:09:39 +0300

    sort

--- client/src/page/Main/MainPage.tsx
+++ client/src/page/Main/MainPage.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MainPage.scss';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 
 function MainPage(): JSX.Element {
   const students = useSelector((store: RootState) => store.students.students);
+  const [filteredPhase, setFilteredPhase] = useState<string | null>(null)
+
+  const sortedStudentsByPhase = [...students].sort((a, b) => a.phase.localeCompare(b.phase));
+  const filterStudentByPhase = (phase: string) => {
+    setFilteredPhase(phase);
+  };
+
   return (
     <div className="MainPage">
       <h1>Main Page</h1>
-      <div className='mapStudents'>
-  {students.map((student)=>(
-    <div key={student.id}>
-    <p>{student.name}</p>
-    <p>{student.phase}</p>
-    </div>
-  ))}
+      <div className="mapStudents">
+        {sortedStudentsByPhase.map((student) => {
+          if (filteredPhase && student.phase !== filteredPhase) {
+            return null;
+          }
+          return (
+            <div key={student.id}>
+              <p>{student.name}</p>
+              <p>phase: {student.phase}</p>
+              <p>Количество благодарностей: {student.count_thank}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <button onClick={()=>filterStudentByPhase('1')}>1 Fhase</button>
+        <button onClick={()=>filterStudentByPhase('2')}>2 Fhase</button>
+        <button onClick={()=>filterStudentByPhase('3')}>3 Fhase</button>
       </div>
     </div>
   );
