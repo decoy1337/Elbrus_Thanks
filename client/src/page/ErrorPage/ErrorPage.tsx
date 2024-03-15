f314cfd3f479a942acd9e03773f07b63171ddcee
Ayur67 <aur67@mail.ru>
2024-03-15 14:49:16 +0300

    errPage done

---
+++ client/src/page/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ErrorPage.scss';
+
+function ErrorPage(): JSX.Element {
+  return (
+    <div className="error__page">
+      <img
+        className="gif"
+        src="https://cdn.dribbble.com/users/1152726/screenshots/2940106/media/653b8215fafb8af439dd67ca33534f56.gif"
+        alt="gifka"
+      />
+    </div>
+  );
+}
+
+export default ErrorPage;
--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const studentsApiroute=require('./api/api.students.router')
+const studentsApiroute = require('./api/api.students.router');
 const apiAuthRouter = require('./api/api.auth.routes');
 
-router.use ('/api/students',studentsApiroute)
+router.use('/api/students', studentsApiroute);
 router.use('/api/auth', apiAuthRouter);
-
 
 module.exports = router;
